687dc8700f01076bb5545d89fbb5582c1355657c
Eury Peña <epena@main.domex.domain>
2022-02-22 17:13:03 -0400

    Query basico cambiado.

--- query-prueba.sql
+++ query-prueba.sql
@@ -1,8 +1,12 @@
-SELECT A.n1 AS Primera, A.n2 AS Segunda, A.n3 AS Tercera, A.nd AS FECHA
-FROM `national` AS A
-INNER JOIN `national` AS B
-ON a.id = B.id
-WHERE A.n1 = B.n1
-AND A.n2 = B.n2
-AND A.n3 = B.n3
-AND A.nd > '2017-06-06'
+SELECT
+    A.n1 AS Primera,
+    A.n2 AS Segunda,
+    A.n3 AS Tercera,
+    A.nd AS FECHA
+FROM
+    `quiniela` AS A
+INNER JOIN `quiniela` AS B
+ON
+    a.id = B.id
+WHERE
+    A.nd > '2019-01-01';
