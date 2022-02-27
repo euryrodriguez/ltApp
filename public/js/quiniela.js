463399c84a6c739e415d6faa66729ac90c8d421e
eury958 <eury958@gmail.com>
2022-02-27 19:08:50 -0400

    Listado de numeros del mes que mas se repitieron.

--- public/js/quiniela.js
+++ public/js/quiniela.js
@@ -1,5 +1,6 @@
 class Quiniela{
     constructor(){
+        this.data = [];
         this.to = getDate('en');
         this.from = getDate('en', 0, 15);
         this.validate = new euvalidate();
@@ -33,7 +34,8 @@ class Quiniela{
                     }
                 } ,
                 "initComplete": function( settings, json ) {
-
+                    selfClass.data = json; 
+                    selfClass.getMostFrequently(json);
                 },
                 "columns": [
                     { "data": "id" },
@@ -63,13 +65,69 @@ class Quiniela{
                         },
                         "width":"15%",
                         "className": ""
+                    },
+                    {
+                        "data":(data)=>{
+                            return `
+                            <a href="#" data-n1="${data.n1}" data-n2="${data.n2}" 
+                            data-n3="${data.n3}" class="btn btn-info btn-details" 
+                            data-toggle="tooltip" data-placement="top" title="Ver más">
+                            <i class="fas fa-angle-down"></i>
+                            </a>
+                            `;
+                        },
+                        "width":"15%",
+                        "className": "text-center"
                     }
                 ]
             });
         }
     }
-    async getNumbersFromTo(from, to){
-        const url = `/quinielas/getNumbers/${from}/${to}`;
+    async getNumbersHistory(numbers){
+        //const url = `/quinielas/getNumbersHistory/{}`;
+
+    }
+    getMostFrequently(data){
+        let numbers = [];
+        let counterObject = {};
+        const $frequentlyList = $('#frequently-list');
+        $frequentlyList.html('');
+        data.forEach(element => {
+            let n1 = element.n1;
+            let n2 = element.n2;
+            let n3 = element.n3;
+            n1 = (n1 == 0) ? 100 : n1;
+            n2 = (n2 == 0) ? 100 : n2;
+            n3 = (n3 == 0) ? 100 : n3;
+            
+            if(counterObject.hasOwnProperty(n1)){
+                counterObject[n1]++;
+            }else{
+                counterObject[n1] = 1;
+            }
+            if(counterObject.hasOwnProperty(n2)){
+                counterObject[n2]++;
+            }else{
+                counterObject[n2] = 1;
+            }
+            if(counterObject.hasOwnProperty(n3)){
+                counterObject[n3]++;
+            }else{
+                counterObject[n3] = 1;
+            }
+        });
+     
+        for(let number in counterObject){
+            let count = counterObject[number];
+            if(count >= 3){
+                numbers.push(number);
+            }
+        }
+        if(numbers.length>0){
+            numbers.forEach(function(number){
+                $frequentlyList.append(`<li class="list-group-item">${number} salió ${counterObject[number]} veces</li>`);
+            });
+        }
     }
 }
 
@@ -84,7 +142,7 @@ document.addEventListener('DOMContentLoaded', function(){
     esperar(200).then(function(){
         $('#btn-query').trigger('click');
     });
-   
+    
     $('#btn-query').on('click', async function(){
         if(quiniela.validate.validateForm($quinielaForm)){
             const $dateFrom = $('#dateFrom');
@@ -105,4 +163,51 @@ document.addEventListener('DOMContentLoaded', function(){
             }
         }
     });
+    
+    $(document).on('click', '.btn-details', function(e){
+        e.preventDefault();
+        const $selector = $(this);
+        const $tdParent = $selector.parent('td');
+        const $trRowParent = $tdParent.parent('tr');
+        const $iconSVG = $selector.find('svg');
+        const iconClassDown = 'fa-angle-down';
+        const $newTr = $trRowParent.next('.newTr');
+        const dataN1 = $selector.data('n1');
+        const dataN2 = $selector.data('n2');
+        const dataN3 = $selector.data('n3');
+        const dataNumbers = {
+            n1: parseInt(dataN1),
+            n2: parseInt(dataN2),
+            n3: parseInt(dataN3)
+        };
+        const combinations = [
+            {  a: dataNumbers.n1, b: dataNumbers.n3 },
+            {  a: dataNumbers.n2, b: dataNumbers.n1 },
+            {  a: dataNumbers.n3, b: dataNumbers.n2 },
+        ];
+        console.log(combinations);
+        if($iconSVG.hasClass(iconClassDown)){
+            $selector.html(`<i class="fas fa-angle-up"></i>`); 
+        }else{
+            $selector.html(`<i class="fas fa-angle-down"></i>`); 
+        }
+
+        if($newTr.length>0){
+           $newTr.toggleClass('d-none');
+        }else{
+            quiniela.getNumbersHistory(dataNumbers);
+            let newTr = `
+            <tr class="newTr">
+            <td colspan="6">
+            <p class="text-wrap">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi impedit odio consequuntur? 
+            Harum maxime eius quo reiciendis provident quam dicta consequatur dolore cumque eos 
+            pariatur inventore voluptate debitis, incidunt maiores!</p>
+            </td>
+            </tr>
+            `;
+            $trRowParent.after(newTr);    
+        }
+        
+    });
+    
 });
